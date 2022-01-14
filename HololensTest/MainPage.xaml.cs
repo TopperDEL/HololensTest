bb975aed8e26de1defccd487ee81b8f6be3aaafb
TopperDEL <github_com@tparth.de>
2022-01-14 12:35:32 +0100

    Projektdateien hinzufügen.

---
+++ HololensTest/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Leere Seite" wird unter https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x407 dokumentiert.
+
+namespace HololensTest
+{
+    /// <summary>
+    /// Eine leere Seite, die eigenständig verwendet oder zu der innerhalb eines Rahmens navigiert werden kann.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool FreeLibrary(IntPtr hModule);
+
+        [DllImport("kernel32", SetLastError = true, CharSet = CharSet.Ansi)]
+        static extern IntPtr LoadLibrary([MarshalAs(UnmanagedType.LPStr)] string lpFileName);
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            Console.WriteLine("Until here everything is fine.");
+
+            //Now load the DLL. Doing it this way leads to the same error as if
+            //the storj_uplink.dll is placed in the Root-Directory (where MSBUILD or what
+            //packages it and loads it from the start which leads to a crash in app.xaml.cs OnLaunched()).
+            var handle = LoadLibrary("mydll/storj_uplink.dll");
+
+            Console.WriteLine("This line crashes.");
+
+            var version = uplink.NET.Models.Access.GetStorjVersion();
+        }
+    }
+}
